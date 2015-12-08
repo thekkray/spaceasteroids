557587d6c65bfe70f55d856400836b59b634d845
Georgiy Padalko <georgiy.padalko@gmail.com>
2015-12-08 23:56:13 +0200

    HighscoreTextInitializer: text initializer for menu scene

---
+++ Assets/HighscoreTextInitializer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent( typeof( Text ) )]
+public class HighscoreTextInitializer : MonoBehaviour
+{
+	void Start()
+	{
+		Text t = this.GetComponent<Text>() as Text;
+		if( t )
+		{
+			t.text = string.Format( t.text, PlayerPrefsHelper.GetHighscore() );
+		}
+		else
+		{
+			Debug.LogError( "Text component not found!" );
+		}
+	}
+}
