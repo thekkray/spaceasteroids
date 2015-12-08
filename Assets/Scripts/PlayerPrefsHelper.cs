4649d86d0f076c58722616c67bfa7d5069501d62
Georgiy Padalko <georgiy.padalko@gmail.com>
2015-12-08 23:34:23 +0200

    PlayerPrefsHelper: highscore

---
+++ Assets/Scripts/PlayerPrefsHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsHelper
+{
+	private static string m_HighscoreKey = "Highscore";
+
+	public static int GetHighscore()
+	{
+		return PlayerPrefs.GetInt( m_HighscoreKey, 0 );
+	}
+
+	public static void SetHighscore( int highscore )
+	{
+		PlayerPrefs.SetInt( m_HighscoreKey, highscore );
+	}
+}
