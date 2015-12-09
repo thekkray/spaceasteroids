2c80710d146eb13c9e66b7b69019266a02d7030c
Georgiy Padalko <georgiy.padalko@gmail.com>
2015-12-09 21:34:28 +0200

    LoadSceneHelper

---
+++ Assets/Scripts/GameScene/HighscoreTextUpdater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent( typeof( Text ) )]
+public class HighscoreTextUpdater : MonoBehaviour
+{
+	private Text m_Text = null;
+
+	void Awake()
+	{
+		m_Text = this.GetComponent<Text>() as Text;
+		if( m_Text == null )
+			Debug.LogError( "Text component not found!" );
+	}
+
+	void OnScoreChanged( int new_score )
+	{
+		if( m_Text )
+			m_Text.text = new_score.ToString();
+	}
+}
---
+++ Assets/Scripts/Utils/LoadSceneHelper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneHelper : MonoBehaviour
+{
+	public void LoadScene( string scene_name )
+	{
+		SceneManager.LoadScene( scene_name );
+	}
+}
