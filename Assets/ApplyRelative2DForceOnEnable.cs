19480c5a6e2449655c6c43c3de1bde5174b1e1ae
Georgiy Padalko <georgiy.padalko@gmail.com>
2015-12-11 00:16:19 +0200

    minor fixes for bullets

---
+++ Assets/ApplyRelative2DForceOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( Rigidbody2D ) )]
+public class ApplyRelative2DForceOnEnable : MonoBehaviour
+{
+	public Vector2 m_RelativeForce = Vector2.zero;
+
+	void OnEnable()
+	{
+		Rigidbody2D rb = this.GetComponent<Rigidbody2D>() as Rigidbody2D;
+
+		Debug.Assert( rb, "Rigidbody2D component not found!" );
+
+		if( rb )
+		{
+			rb.AddRelativeForce( m_RelativeForce );
+		}
+	}
+}
