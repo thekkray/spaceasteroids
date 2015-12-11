cda24498e883150569025f085f4fdd303e48cc7f
Georgiy Padalko <georgiy.padalko@gmail.com>
2015-12-11 19:16:44 +0200

    moving of rocks

---
+++ Assets/ApplyRandom2DForceOnEnable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyRandom2DForceOnEnable : MonoBehaviour
+{
+	public float m_ForceMagnitude = 0.00f;
+
+	void OnEnable()
+	{
+		Rigidbody2D rb = this.GetComponent<Rigidbody2D>() as Rigidbody2D;
+
+		Debug.Assert( rb, "Rigidbody2D component not found!", this.gameObject );
+
+		if( rb )
+		{
+			rb.AddRelativeForce( Random.insideUnitCircle * m_ForceMagnitude );
+		}
+	}
+}
